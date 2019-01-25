681c7d7d6e3f7542567dc79e995cdee520d91b70
Shrestha <shrestha.rishan99@gmail.com>
2019-01-25 17:05:48 +0545

    new change

--- app/src/main/java/com/example/amuntimilsina/bideshisawari/fragments/HomeFragment.java
+++ app/src/main/java/com/example/amuntimilsina/bideshisawari/fragments/HomeFragment.java
@@ -10,12 +10,16 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.amuntimilsina.bideshisawari.HomeActivity;
 import com.example.amuntimilsina.bideshisawari.R;
 import com.example.amuntimilsina.bideshisawari.StartPageActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 
 public class HomeFragment extends Fragment {
@@ -23,22 +27,45 @@ public class HomeFragment extends Fragment {
     ImageView logOutBtn;
     FirebaseAuth auth;
     FirebaseAuth.AuthStateListener authStateListener;
+    FrameLayout frameLayout;
+    LinearLayout parks,parking,resturants,places;
+    TextView parkstext,parkingtext,resturanttext,placestext;
+    CircularImageView parkImage,parkingImage,resturantImage,placeImage;
+    View line;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        line=view.findViewById(R.id.line);
+        parkImage=view.findViewById(R.id.park_icon);
+        parkingImage=view.findViewById(R.id.parking_icon);
+        resturantImage=view.findViewById(R.id.resturant_icon);
+        placeImage=view.findViewById(R.id.places_icon);
         logOutBtn = view.findViewById(R.id.logOutBtn);
-        auth = FirebaseAuth.getInstance();
-
-        authStateListener = new FirebaseAuth.AuthStateListener() {
+        frameLayout = view.findViewById(R.id.home_frame);
+        parks=view.findViewById(R.id.parks);
+        places=view.findViewById(R.id.places);
+        resturants=view.findViewById(R.id.resturants);
+        parking=view.findViewById(R.id.parking);
+        parkingtext=view.findViewById(R.id.parking_text);
+        placestext=view.findViewById(R.id.places_text);
+        resturanttext=view.findViewById(R.id.resturants_text);
+        parkstext=view.findViewById(R.id.parks_text);
+        parks.setOnClickListener(tabclick);
+        places.setOnClickListener(tabclick);
+        resturants.setOnClickListener(tabclick);
+        parking.setOnClickListener(tabclick);
+        // auth = FirebaseAuth.getInstance();
+        /*authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if(firebaseAuth.getCurrentUser() == null){
                     startActivity(new Intent(getActivity(),StartPageActivity.class));
                 }
             }
-        };
+        };*/
+        getActivity().getSupportFragmentManager().beginTransaction().add(R.id.home_frame,new HomeDefaultFragment()).commit();
 
         logOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,25 +77,87 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
-    public void DisplayLogoutDialog(){{
-        new AlertDialog.Builder(getContext())
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setTitle("Log Out")
-                .setMessage("Are you sure you want to Log out?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        auth.signOut();
-                    }
-                })
-                .setNegativeButton("No",null)
-                .show();
-    }
+    public void DisplayLogoutDialog() {
+        {
+            new AlertDialog.Builder(getContext())
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Log Out")
+                    .setMessage("Are you sure you want to Log out?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            auth.signOut();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        auth.addAuthStateListener(authStateListener);
+//        auth.addAuthStateListener(authStateListener);
     }
+
+    public View.OnClickListener tabclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            if (view.getId() == R.id.parks) {
+                parkImage.setBorderColor(getResources().getColor(R.color.ImageBorder));
+                parkingImage.setBorderColor(getResources().getColor(R.color.white));
+                resturantImage.setBorderColor(getResources().getColor(R.color.white));
+                placeImage.setBorderColor(getResources().getColor(R.color.white));
+                parkstext.setTextColor(getResources().getColor(R.color.black));
+                line.setBackgroundColor(getResources().getColor(R.color.black));
+                resturanttext.setTextColor(getResources().getColor(R.color.white));
+                parkingtext.setTextColor(getResources().getColor(R.color.white));
+                placestext.setTextColor(getResources().getColor(R.color.white));
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.home_frame, new ParksFragment()).commit();
+
+            } else if (view.getId() == R.id.places) {
+                parkImage.setBorderColor(getResources().getColor(R.color.white));
+                parkingImage.setBorderColor(getResources().getColor(R.color.white));
+                resturantImage.setBorderColor(getResources().getColor(R.color.white));
+                placeImage.setBorderColor(getResources().getColor(R.color.blue));
+                line.setBackgroundColor(getResources().getColor(R.color.black));
+                parkstext.setTextColor(getResources().getColor(R.color.white));
+                resturanttext.setTextColor(getResources().getColor(R.color.white));
+                parkingtext.setTextColor(getResources().getColor(R.color.white));
+                placestext.setTextColor(getResources().getColor(R.color.ImageBorder));
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.home_frame, new PlacesFragment()).commit();
+
+            } else if (view.getId() == R.id.parking) {
+                parkImage.setBorderColor(getResources().getColor(R.color.white));
+                parkingImage.setBorderColor(getResources().getColor(R.color.ImageBorder));
+                resturantImage.setBorderColor(getResources().getColor(R.color.white));
+                placeImage.setBorderColor(getResources().getColor(R.color.white));
+                parkstext.setTextColor(getResources().getColor(R.color.white));
+                resturanttext.setTextColor(getResources().getColor(R.color.white));
+                parkingtext.setTextColor(getResources().getColor(R.color.ImageBorder));
+                placestext.setTextColor(getResources().getColor(R.color.white));
+                line.setBackgroundColor(getResources().getColor(R.color.ImageBorder));
+
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.home_frame, new ParkingFragment()).commit();
+
+            } else if (view.getId() == R.id.resturants) {
+                parkImage.setBorderColor(getResources().getColor(R.color.white));
+                parkingImage.setBorderColor(getResources().getColor(R.color.white));
+                resturantImage.setBorderColor(getResources().getColor(R.color.ImageBorder));
+                placeImage.setBorderColor(getResources().getColor(R.color.white));
+                parkstext.setTextColor(getResources().getColor(R.color.white));
+                resturanttext.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                line.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                parkingtext.setTextColor(getResources().getColor(R.color.white));
+                placestext.setTextColor(getResources().getColor(R.color.white));
+
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.home_frame, new ResturantsFragment()).commit();
+
+            }
+
+
+        }
+    };
+
 }
--- app/src/main/java/com/example/amuntimilsina/bideshisawari/fragments/TransportFragment.java
+++ app/src/main/java/com/example/amuntimilsina/bideshisawari/fragments/TransportFragment.java
@@ -9,13 +9,60 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.amuntimilsina.bideshisawari.R;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.ImageButton;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class TransportFragment extends Fragment {
+public class TransportFragment extends Fragment implements OnMapReadyCallback {
+
+
+    GoogleMap Map;
+    ImageButton bringToPositionBtn;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transport,container,false);
 
+        bringToPositionBtn = view.findViewById(R.id.bringToPositionBtn);
+
+        bringToPositionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Add your location
+//                LatLng Ktm = new LatLng(27.7172, 85.3240);
+//                Map.moveCamera(CameraUpdateFactory.newLatLngZoom(Ktm, 16));
+            }
+        });
+
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        if(mapFragment == null){
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map,mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Map = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        Map = googleMap;
+        LatLng Ktm = new LatLng(27.7172, 85.3240);
+        Map.moveCamera(CameraUpdateFactory.newLatLngZoom(Ktm, 16));
+        Map.addMarker(new MarkerOptions().position(Ktm).title("Marker in Sydney"));
     }
 }
