550fe41698f86a6d05e632111e59cbc7d82a6864
Shrestha <shrestha.rishan99@gmail.com>
2019-01-25 17:10:44 +0545

    new change

---
+++ app/src/main/java/com/example/amuntimilsina/bideshisawari/fragments/HomeDefaultFragment.java
@@ -0,0 +1,18 @@
+package com.example.amuntimilsina.bideshisawari.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.amuntimilsina.bideshisawari.R;
+
+public class HomeDefaultFragment extends Fragment {
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_default, container, false);
+            return view;
+        }
+}
